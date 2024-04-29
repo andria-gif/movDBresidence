90f8d12c6c6e9f0a78026817d15f88d89e45b2a0
Vinicius <viiniciuaramos88@gmail.com>
2024-04-29 10:52:46 -0300

    feat: criacao serie viewController com collectionView

---
+++ Movies/Models/series.swift
@@ -0,0 +1,15 @@
+//
+//  series.swift
+//  Movies
+//
+//  Created by ios-manha-05 on 24/04/24.
+//
+
+import Foundation
+
+struct Serie {
+    let id: String
+    let title: String
+    let released: String?
+    let language: String?
+}
