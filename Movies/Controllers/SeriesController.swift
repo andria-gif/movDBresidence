a976468abb87f5800a7c95415778a867647df288
Vinicius <viiniciuaramos88@gmail.com>
2024-06-12 09:52:09 -0300

    feat: add searchController

--- Movies/Controllers/SeriesController.swift
+++ Movies/Controllers/SeriesController.swift
@@ -6,7 +6,26 @@
 //
 
 import Foundation
+import UIKit
 
-class SeriesController: UIViewController {
+class SeriesController: UIViewController	 {
+    let searchController = UISearchController(searchResultsController: nil)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    func setupView(){
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Procurar por séries"
+        navigationItem.searchController = searchController
+    }
+}
 
+extension SeriesController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print(searchController.searchBar.text!)
+        print("Quem está me controlando " + "\(searchController.searchResultsUpdater!)")
+    }
+    
+    
 }
