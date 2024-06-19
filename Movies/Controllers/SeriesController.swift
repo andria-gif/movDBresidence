6c228d94a1d7cc8278135f2ba94a88fbad89d891
ios-manha-08 <ios-manha-08@MacBook-Pro-de-Administrador.local>
2024-06-19 10:59:48 -0300

    refactor : modularizar criação da lista de séries

--- Movies/Controllers/SeriesController.swift
+++ Movies/Controllers/SeriesController.swift
@@ -14,41 +14,53 @@ class SeriesController: UIViewController {
     
     var dataMock = DataMock()
     
+    private var numberOfItemsInRow = 3
+    private var minimumSpacing = 5
+    private var edgeInsetPadding = 10
     
     override func viewDidLoad(){
         super.viewDidLoad()
         collectionView.dataSource = self
+        collectionView.delegate = self
         dataMock.insertMock()
-        
     }
-    
-    
 }
 
-extension SeriesController : UICollectionViewDataSource{
+extension SeriesController : UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return dataMock.series.count
-        
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "defaultSerieCell", for : indexPath) as! SerieCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "defaultSerieCell", for: indexPath) as! SerieCollectionViewCell
         
         let serie = dataMock.series[indexPath.row]
         cell.background.backgroundColor = UIColor.orange
         cell.title.text = serie.title
         cell.layer.cornerRadius = 16
-       // cell.title.text = categories[indexPath.row]
-        
-        //cell.layer.cornerRadius = 16
-        
-       
-        
          
-        //return cell
-        return UICollectionViewCell()
+        return cell
     }
-    
-    
+}
+
+
+extension SeriesController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset = UIEdgeInsets(top: 40, left: 40, bottom: 40, right: 40)
+        edgeInsetPadding = Int(inset.left + inset.right)
+        return inset
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(minimumSpacing)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(minimumSpacing)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (Int(UIScreen.main.bounds.size.width) - (numberOfItemsInRow - 1) * minimumSpacing - edgeInsetPadding) / numberOfItemsInRow
+        return CGSize(width: width, height: width)
+    }
 }
